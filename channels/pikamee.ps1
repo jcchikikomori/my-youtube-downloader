ef853ccc6761cefef3b8e0971c633579e23756a6
John Cyrill Corsanes <jccorsanes@protonmail.com>
2023-03-28 13:02:32 +0800

    Pikamee

--- channels/pikamee.ps1
+++ channels/pikamee.ps1
@@ -1,9 +1,9 @@
 # Enter the channel URL and output folder name
-$channel_url = "https://www.youtube.com/@Pikamee"
-$output_folder = ".\vods\pikamee"
+$channel_url = "https://www.youtube.com/channel/UCajhBT4nMrg3DLS-bLL2RCg"
+$output_folder = ".\vods"
 
 # Create the output folder if it does not exist
 New-Item -ItemType Directory -Force -Path $output_folder | Out-Null
 
 # Download the channel
-youtube-dlc --write-info-json --write-description --write-thumbnail --playlist-end 99999 --output "$output_folder/%(uploader)s/%(title)s.%(ext)s" "$channel_url"
+youtube-dlc --write-info-json --write-description --write-thumbnail --playlist-end 99999 --output "$output_folder/%(uploader)s/%(title)s.%(ext)s" "$channel_url" --ignore-errors
